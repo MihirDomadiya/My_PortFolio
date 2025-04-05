b9452de0f82345a58433bbcb955c71177dc33ed8
MihirDomadiya <mihirdomadiya55@gmail.com>
2025-04-06 00:36:13 +0530

    MD | Add Headre

--- my-portfolio/src/app/layout.js
+++ my-portfolio/src/app/layout.js
@@ -3,7 +3,7 @@ import Header from "@/components/header";
 
 export const metadata = {
   title: "Mihir's Portfolio",
-  description: "My personal portfolio website built with Next.js",
+  description: "",
 };
 
 export default function RootLayout({ children }) {
@@ -11,25 +11,16 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />=
         <title>{metadata.title}</title>
         <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css"
           rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/vs.min.css"
-        />
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7"
           crossOrigin="anonymous"
         ></link>
       </head>
-      <body>
+      <body className="bg-black">
         <Header />
         {children}
       </body>
--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,9 +1,42 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Header() {
   return (
-    <div className="text-danger">
-      Hyszdcnv df v sadzvckavdwcvfszcn nsdbfvcuewarvc zsdcv v
-    </div>
+    <nav className="col-6 mx-auto navbar navbar-expand-lg">
+      <Link className="navbar-brand text-white" href="/">
+        Mihir
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link text-white" href="#">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
+              Features
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
+              Pricing
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
